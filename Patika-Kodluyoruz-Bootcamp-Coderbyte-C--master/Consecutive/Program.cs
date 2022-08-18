37942f781be2485eacae7103f77bd4bfbf7036ee
Yunus Emre Topcu <yunusemre_topcu@hotmail.com>
2022-08-18 13:34:35 +0300

    week-4

---
+++ Patika-Kodluyoruz-Bootcamp-Coderbyte-C--master/Consecutive/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+Have the function Consecutive(**arr**) take the array of integers 
+stored in **arr** and return the minimum number of integers needed 
+to make the contents of **arr** consecutive from the lowest number to 
+the highest number. For example: If **arr** contains [4, 8, 6] then the 
+output should be **2** because two numbers need to be added to the array 
+(5 and 7) to make it a consecutive array of numbers from **4** to **8**.
+Negative numbers may be entered as parameters and no array will have 
+less than 2 elements.
+
+### Examples
+Input: new int[] {5,10,15}
+Output: 8
+
+Input: new int[] {-2,10,4}
+Output: 10
+*/
+using System;
+
+class MainClass
+{
+    public static int Consecutive(int[] arr)
+    {
+        int bigVal = arr[0];
+        int lowVal = arr[0];
+        int counter = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > bigVal)
+            {
+                bigVal = arr[i];
+            }
+            else if (arr[i] < lowVal)
+            {
+                lowVal = arr[i];
+            }
+        }
+
+        for (int i = (lowVal + 1); i < bigVal; i++)
+        {
+            for (int j = 0; j < arr.Length; j++)
+            {
+                if (arr[j] == i)
+                {
+                    counter -= 1;
+                }
+            }
+            counter += 1;
+        }
+
+        return counter;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Input: { 5, 10, 15 }");
+        Console.WriteLine("Output: " + Consecutive(new int[] { 5, 10, 15 }));
+        Console.WriteLine("Input: { -2, 10, 4 }");
+        Console.WriteLine("Output: " + Consecutive(new int[] { -2, 10, 4 }));
+        Console.WriteLine("Input: { -4, 15, 3, 2, 6 }");
+        Console
+            .WriteLine("Output: " + Consecutive(new int[] { -4, 15, 3, 2, 6 }));
+        Console.WriteLine("Input: { 5, 5, 5 }");
+        Console.WriteLine("Output: " + Consecutive(new int[] { 5, 5, 5 }));
+    }
+}
---
+++ Patika-Kodluyoruz-Bootcamp-Coderbyte-C--master/SnakeCase/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Have the function SnakeCase(**str**) take the **str** parameter being 
+passed and return it in proper snake case format where each word is 
+lowercased and separated from adjacent words via an underscore. 
+The string will only contain letters and some combination of delimiter 
+punctuation characters separating each word.For example: if **str** is 
+"BOB loves-coding" then your program should return the string 
+**bob_loves_coding**.
+
+### Examples
+Input: "cats AND*Dogs-are Awesome"
+Output: cats_and_dogs_are_awesome
+
+Input: "a b c d-e-f%g"
+Output: a_b_c_d_e_f_g
+*/
+using System;
+using System.Linq;
+
+class MainClass
+{
+    public static string SnakeCase(string str)
+    {
+        char[] delimiterChars = { '.', ',', '-', '%', '+', ' ', '*' };
+        str = str.ToLower();
+        string[] sol = str.Split(delimiterChars);
+        string solStr =
+            String.Join(" ", sol.Select(i => i.ToString()).ToArray());
+        solStr = solStr.Replace(" ", "_");
+        return solStr;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(SnakeCase(Console.ReadLine()));
+    }
+}
